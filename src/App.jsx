4178e9e063632e01ee63982e5143fe32b5753787
nathalieMalsh <nmalysheva99@mail.ru>
2026-01-23 15:58:30 +0300

    add stopVideo action into playerMachine

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react'
 import classes from './App.module.css'
 import { Modal, Button } from 'antd'
 import { PlayCircleOutlined, ShrinkOutlined, ArrowsAltOutlined, PauseOutlined, CaretRightOutlined } from '@ant-design/icons'
@@ -5,16 +6,22 @@ import { useMachine } from '@xstate/react'
 import { playerMachine } from './playerMachine'
 import ReactPlayer from 'react-player'
 
-
 const App = () => {
-  const [state, send] = useMachine(playerMachine)
-  console.log('Current state:', state)
+  const playerRef = useRef(null)
+  const [state, send] = useMachine(
+    playerMachine.provide({
+      actions: {
+        stopVideo: () => {
+          const player = playerRef.current
+          player.pause()
+        }
+      }
+    })
+  )
 
   const isPlayerOpened = state.matches('opened')
   const isVideoPlaying = state.matches({ opened: { video: 'playing' } })
   const isModalFull = state.matches({ opened: { size: 'full' } })
-
-  console.log('isVideoPlaying:', isVideoPlaying)
 
   return (
     <div className={classes.app}>
@@ -24,9 +31,7 @@ const App = () => {
           open={isPlayerOpened}
           title='PLAYER'
           width={isModalFull ? 1000 : 500}
-          onCancel={() => {
-            send({ type: 'pause' })
-          }}
+          onCancel={() => send({ type: 'close' })}
           footer={[
             <Button shape='circle' key='size'
               icon={isModalFull ? <ShrinkOutlined /> : <ArrowsAltOutlined />}
@@ -38,6 +43,7 @@ const App = () => {
           ]}
         >
           <ReactPlayer
+            ref={playerRef}
             src='https://cdn.flowplayer.com/d9cd469f-14fc-4b7b-a7f6-ccbfa755dcb8/hls/383f752a-cbd1-4691-a73f-a4e583391b3d/playlist.m3u8'
             playing={isVideoPlaying}
             loop={true}
--- src/playerMachine.js
+++ src/playerMachine.js
@@ -11,6 +11,7 @@ export const playerMachine = createMachine({
     },
     opened: {
       type: 'parallel',
+      exit: 'stopVideo',
       on: {
         close: 'closed'
       },
